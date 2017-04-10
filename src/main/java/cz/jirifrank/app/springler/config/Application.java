2403b2f2dc2cfb3919153a4fa5e47a3b91c25f82
BaRs <info@jirifrank.eu>
2017-04-10 20:37:37 +0200

    Datasource added

--- src/main/java/cz/jirifrank/app/springler/config/Application.java
+++ src/main/java/cz/jirifrank/app/springler/config/Application.java
@@ -3,9 +3,15 @@ package cz.jirifrank.app.springler.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.sql.DataSource;
 
 /**
  * Main configuration file of app
@@ -24,6 +30,13 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	@Primary
+	@ConfigurationProperties(prefix = "spring.datasource")
+	public DataSource dataSource() {
+		return DataSourceBuilder.create().build();
 	}
 
 }
