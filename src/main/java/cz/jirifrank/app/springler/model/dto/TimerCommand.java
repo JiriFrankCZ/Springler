dba5f3da71495dfb4e954469383cdd05e48760e3
JiriFrank <info@jirifrank.eu>
2017-04-11 14:07:15 +0200

    Updated version of jackson

--- src/main/java/cz/jirifrank/app/springler/model/dto/TimerCommand.java
+++ src/main/java/cz/jirifrank/app/springler/model/dto/TimerCommand.java
@@ -1,11 +1,14 @@
 package cz.jirifrank.app.springler.model.dto;
 
+import lombok.Data;
+
 import java.time.LocalTime;
 import java.util.List;
 
 /**
  * Created by FrankJ on 10.4.2017.
  */
+@Data
 public class TimerCommand {
 	private List<LocalTime> timeList;
 }
