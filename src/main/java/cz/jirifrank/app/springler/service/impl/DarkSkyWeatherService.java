cf2caff401050d860754bb690cc64b7c9a80053d
JiriFrank <info@jirifrank.eu>
2017-07-29 15:36:11 +0200

    Update DarkSkyWeatherService.java
    asd

--- src/main/java/cz/jirifrank/app/springler/service/impl/DarkSkyWeatherService.java
+++ src/main/java/cz/jirifrank/app/springler/service/impl/DarkSkyWeatherService.java
@@ -65,11 +65,11 @@ public class DarkSkyWeatherService implements WeatherService {
 
 		if(responseEntity.getStatusCode().is2xxSuccessful()) {
 			log.info("Weather info request was successful.");
-			log.debug(responseEntity);
-			log.debug(responseEntity.getBody());
-			log.debug(responseEntity.getBody().getDaily().getData());
-			log.debug(responseEntity.getBody().getDaily().getData().get(0));
-			log.debug(responseEntity.getBody().getDaily().getData().get(0).getPrecipType());
+			log.debug("log",responseEntity);
+			log.debug("log",responseEntity.getBody());
+			log.debug("log",responseEntity.getBody().getDaily().getData());
+			log.debug("log",responseEntity.getBody().getDaily().getData().get(0));
+			log.debug("log",responseEntity.getBody().getDaily().getData().get(0).getPrecipType());
 			Data data = responseEntity.getBody().getDaily().getData().get(0);
 
 			WeatherInfo weatherInfo = new WeatherInfo();
