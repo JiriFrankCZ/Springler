6cfaa93d0fc0abfec3844cfe7b29a6eb2c7e53ea
JiriFrank <info@jirifrank.eu>
2017-08-11 10:43:24 +0200

    Update IrrigationController.java

--- src/main/java/cz/jirifrank/app/springler/controller/IrrigationController.java
+++ src/main/java/cz/jirifrank/app/springler/controller/IrrigationController.java
@@ -54,7 +54,7 @@ public class IrrigationController {
 		log.info("Rain probability request.");
 
 		WeatherInfo actualForecast = weatherService.getActualForecast();
-		boolean rain = actualForecast.getRainProbability() > 60;
+		boolean rain = actualForecast.getRainProbability() > 0.6;
 
 		log.info("Probability resolved : {}", rain);
 
