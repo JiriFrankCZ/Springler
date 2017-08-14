d4ed2984c930278417e391d7c25c891bccf4fc08
BaRs <info@jirifrank.eu>
2017-08-14 19:08:07 +0200

    Major update to handle logging, and dynamic config

---
+++ src/main/java/cz/jirifrank/app/springler/model/statics/Action.java
@@ -0,0 +1,10 @@
+package cz.jirifrank.app.springler.model.statics;
+
+public enum Action {
+    STANDARD_WATERING,
+    MANUAL_WATERING,
+    EMERGENCY_WATERING,
+    WATERING_BYPASSED_BY_WEATHER,
+    ALERT,
+    REPORTING
+}
